fd79f2ffce4848d03e5b438396a91234e8cab70f
00264126 <00264126@uca.edu.sv>
2026-04-29 13:56:19 -0600

    Correciones post-clase

--- IF/rpd2.cpp
+++ IF/rpd2.cpp
@@ -14,9 +14,31 @@ int main() {
     }
     else {
         cout << "Vuelve a intentarlo";
-        int contador = intentos -1;
+        int contador = intentos - 1;
+        cout << "Le restan " << contador <<" intentos" << endl;
+            
+        cout << "Ingrese su usario y contrasena ";
+    cin >> user >> contra;
+    if (user == "name" && contra == 1234) {
+        cout << "Bienvenido usario " << user << endl;
+    }
+    else {
+        cout << "Vuelve a intentarlo ";
+        int contador = intentos -2;
+        cout << "Le restan " << contador <<" intentos" << endl;
+    
+        cin >> user >> contra;
+    if (user == "name" && contra == 1234) {
+        cout << "Bienvenido usario " << user << endl;
+    }
+    else {
+        cout << "Vuelve a intentarlo " << endl;
+        int contador = intentos -3;
+        cout << "Le restan " << contador <<" intentos" << endl;
         
     }    
     
+    }    
+}
     return 0;
 }
--- IF/rpm5.cpp
+++ IF/rpm5.cpp
@@ -6,25 +6,25 @@ float peso , billetera =1000,cobro;
 cout << "Ingrese el peso de su paquete " << endl ;
 cin >> peso;
 if (peso <= 5 && peso > 0){
-    cout << "Su tarifa de envio es de 5 $";
+    cout << "Su tarifa de envio es de 5 $ " << endl;
    cobro = billetera -5;
-   cout << "su billetera tiene un fondo de " << cobro << "dolares" << endl;
+   cout << "Su billetera tiene un fondo restante de " << cobro << " dolares" << endl;
  }
 
  else if (peso >= 6 && peso <= 10) {
-    cout << "Su tarifa a pagar es de 10 dolares" ;
+    cout << "Su tarifa a pagar es de 10 dolares " << endl;
    cobro = billetera - 10;
-   cout << "su billetera tiene un fondo de " << cobro << "dolares" << endl;
+   cout << "Su billetera tiene un fondo restante de " << cobro << " dolares" << endl;
  }
  
  else if (peso >= 15) {
-    cout << "Su total a pagar es de 15 dolares";
+    cout << "Su total a pagar es de 15 dolares" << endl;
     cobro = billetera - 15;
-    cout << "su billetera tiene un fondo de " << cobro << "dolares" << endl;
+    cout << "Su billetera tiene un fondo restante de " << cobro << " dolares" << endl;
 
  }
    else{
-      cout << " valor invalido";
+      cout << " Valor invalido";
    }
     return 0;
 }

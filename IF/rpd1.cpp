169b4a3539d19e3bb756503c01d165353999727f
00264126 <00264126@uca.edu.sv>
2026-04-29 08:47:44 -0600

    Moviendo arch y For

--- IF/rpd1.cpp
+++ IF/rpd1.cpp
@@ -2,8 +2,19 @@
 using namespace std;
 
 main (){
+int v;
+cout << "Ingrese su velocidad" << endl;
+cin >> v;
+if (v > 60  &&  v <= 80 ) {
+    cout << "Usted recibira una multa de 100 $ ";
 
-
+}
+else if(v > 80){
+    cout << "Usted recibira una multa de 150 $ ";
+}
+else {
+    cout << "Usted esta dentro del limite de velocidad permitido";
+}
 
     return 0;
 }
